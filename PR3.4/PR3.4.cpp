477a76b64e40d9e0ad909cf25114dd1ff0311b9b
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-05 22:49:07 +0300

    first master

---
+++ PR3.4/PR3.4.cpp
@@ -0,0 +1,22 @@
+﻿// Lab_03_4.cpp
+// < Молотюк Софія >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 16
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	// розгалуження в повній формі
+	if ((y <= x && y <= -pow(x, 2) + 2 && y >= 0) ||
+		(y <= -pow(x, 2) + 2 && y <= 0 && y >= x))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
